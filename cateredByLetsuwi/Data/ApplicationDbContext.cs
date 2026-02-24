ac11bfc653211132fced86c7aebeed0799026478
Marothi <166597253+Marothi-Mohale@users.noreply.github.com>
2026-02-24 08:37:43 +0000

    Added Services CRUD with EF Core SQLite

---
+++ cateredByLetsuwi/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using cateredByLetsuwi.Models;
+
+namespace cateredByLetsuwi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Service> Services { get; set; }
+    }
+}
